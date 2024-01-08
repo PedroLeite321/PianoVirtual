a2d50e805650dd40fa98882980dc42131c09a338
pedro <pk.bleite@gmail.com>
2024-01-08 00:58:17 -0300

    keyboard key down event added /  evento de tecla apertada adicionado

--- scripts/engine.js
+++ scripts/engine.js
@@ -7,12 +7,20 @@ const gameSetup  = () => {
         //load audio to memory
         audio.src = `./tunes/${key}.wav`;
         audio.play();
-        
+        const clickedKey = document.querySelector(`[data-key="${key}"]`);
+        clickedKey.classList.add("active");
+        setTimeout(() =>    {
+            clickedKey.classList.remove("active")
+        }, 150);
     };
     pianoKeys.forEach((key) => {
         //data set is used to access values.
         console.log(key.dataset.key);
         key.addEventListener("click", () => {playSound(key.dataset.key)} );
     })
+    
+    document.addEventListener("keydown", (e) => {
+        playSound(e.key);
+    } )
 }
 gameSetup();
